200c812b34fa6752bed91fb4f88efe9fd3499b99
Doug Hanley <douglasbhanley@gmail.com>
2018-06-20 13:28:01 +0900

    bbc subsample

--- bbc/process.py
+++ bbc/process.py
@@ -1,6 +1,12 @@
 import os
 import glob
+import shutil
+import numpy as np
 
+nsamp = 50
+state = np.random.RandomState(94395245)
+
+# convert to fuzzy format and flatten
 for fpath in glob.glob('raw/*/*'):
     fdir, fname = os.path.split(fpath)
     fbase, fext = os.path.splitext(fname)
@@ -11,3 +17,9 @@ for fpath in glob.glob('raw/*/*'):
     text = f'!{title} #{tag}\n\n{body}'
     with open(f'docs/{tag}_{fbase}', 'w+') as fid:
         fid.write(text)
+
+# draw random sample of 100 articles
+articles = glob.glob('docs/*')
+for fpath in state.choice(articles, size=nsamp, replace=False):
+    fdir, fname = os.path.split(fpath)
+    shutil.copy2(fpath, f'docs1/{fname}')
